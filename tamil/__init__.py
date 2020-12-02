0cb3fc56587f34b121ffd1c4be0e851610163081
rajkumar palani <rajkumarpalani07@gmail.com>
2020-12-02 06:11:03 +0530

    commit

--- tamil/__init__.py
+++ tamil/__init__.py
@@ -1,13 +1,8 @@
 # encoding: utf-8
 # (C) 2001 - 2020 Rajkumar Palani <rajkumarpalani07@gmail.com>
 
-
 from . import tstring
 from . import tace16
 from . import utf8
 from . import tcutil
 from . import tcharacter
-
-
-
-
