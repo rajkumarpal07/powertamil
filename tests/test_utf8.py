edb2a4c611f7d7255225b40ee4497edf3881fd7c
Raj <46222777+rajkumarpal07@users.noreply.github.com>
2020-12-02 05:39:42 +0530

    Create test_utf8.py

---
+++ tests/test_utf8.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# (C) 2001 - 2020 Rajkumar Palani <rajkumarpalani07@gmail.com>
+
+import pytest
+
+from tamil import utf8
+
+def test_get_tamil_utf_count():
+    expected = 364
+    assert(utf8.get_tamil_utf_count() == expected)
+
+
+def test_get_tamil_utf():
+    expected = 364
+    assert(len(utf8.get_tamil_utf()) == expected)
+
+    temp1 = list(filter(lambda x: (x == 'அ'), utf8.get_tamil_utf()))
+    assert(('அ' in temp1[0]) == True)
+
+
+def test_validate_char():
+    input1 = "ட"
+    assert(utf8.validate_char(input1) == True)
+    input2 = 'அ'
+    assert(utf8.validate_char(input2) == True)
+
+
+def test_validate_char_failing():
+    input1 = 1000
+    with pytest.raises(TypeError, match="Input value must be unicode string"):
+        utf8.validate_char(input1)
+
+    input2 = ''
+    with pytest.raises(ValueError, match="Input value is empty"):
+        utf8.validate_char(input2)
+
+    input3 = 'Ж'
+    with pytest.raises(ValueError, match="Input value is not a valid utf8 tamil character"):
+        utf8.validate_char(input3)
+
+
